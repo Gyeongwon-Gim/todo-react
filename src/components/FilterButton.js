2ba17ce38f747ebde6a437780269c02866d6b762
GYEONGWON GIM <gyeongwon7986@gmail.com>
2022-07-25 17:32:09 +0900

    add / delete function

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,14 +1,12 @@
 import React from "react";
 
-function FilterButton(){
+function FilterButton(props){
     return (
-        <div className="filters btn-group stack-exception">
         <button type="button" className="btn toggle-btn" aria-pressed="true">
           <span className="visually-hidden">Show </span>
-          <span>All</span>
+          <span>{props}</span>
           <span className="visually-hidden"> tasks</span>
         </button>
-      </div>
     );
 }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,21 @@
 // TODO:나머지 요소 컴포넌트 만들기
-import React from "react";
+import React, { useState } from "react";
 
-export default function Form( ) {  
-    return(
-      <form>
+export default function Form(props) {  
+  const [name, setName] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.addTask(name);
+    setName("");
+  }  
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+  
+  return(
+      <form onSubmit={handleSubmit}>
         <h2 className="label-wrapper">
           <label htmlFor="new-todo-input" className="label__lg">
             What needs to be done?
@@ -15,6 +27,8 @@ export default function Form( ) {
           className="input input__lg"
           name="text"
           autoComplete="off"
+          value={name}
+          onChange={handleChange}
         />
         <button type="submit" className="btn btn__primary btn__lg">
           Add
